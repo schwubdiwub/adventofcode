07281f2290e57993264eed53a09809165fd42d49
Leonhard Grote <leonhard.grote@udo.edu>
2022-12-03 01:15:34 +0100

    day1 part2 solved

--- day1/calories.cpp
+++ day1/calories.cpp
@@ -34,7 +34,18 @@ int main()
     auto it = std::ranges::max_element(elfs, {}, &Elf::calories);
     auto index = std::distance(elfs.begin(), it);
 
-    std::cout << std::format("Elf #{} is carring {} food items with a total of {} calories.\n", index, elfs[index].foods.size(), elfs[index].calories());
+    std::cout << std::format("Elf #{} is carrying {} food items with a total of {} calories.\n", index, elfs[index].foods.size(), elfs[index].calories());
+
+    std::cout << "\nPart 2:\n";
+
+    std::ranges::partial_sort(elfs, elfs.begin() + 3, std::greater{}, &Elf::calories);
+
+    uint64_t sum = 0;
+    for(int i = 0; i < 3; ++i) {
+        sum += elfs[i].calories();
+    }
+    std::cout << std::format("The three elfs with the most calories are carrying {}, {} and {} calories. That are in total {} calories.\n",
+                             elfs[0].calories(), elfs[1].calories(), elfs[2].calories(), sum);
 
     return 0;
 }
